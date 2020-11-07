09eb9cbe352dae27e7a30bb4300b0aa4c8d7e352
leafar-Santos <leafarccomputacao@gmail.com>
2020-11-07 19:40:13 -0300

    Createde book repository class

---
+++ src/main/java/com/santosrafael/bookstoremanager/respository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.santosrafael.bookstoremanager.respository;
+
+import com.santosrafael.bookstoremanager.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository <Book, Long> {
+
+
+
+
+
+
+}
